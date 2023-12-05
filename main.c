2b76fdb983b2e4dd8c45c47985dc100e1d2db578
Pedro Medina <pedromstracieri@gmail.com>
2023-12-05 11:24:29 -0300

    first come first saved

--- main.c
+++ main.c
@@ -9,21 +9,26 @@ typedef struct Node_{
 }Node;
 
 // insere o novo processo na fila
-Node *insereProcesso(Node *begin, Node *no) {
-    no->tempo = rand() % 20 + 1;
-    Node *ant = begin;
-    Node *pont = begin->prox;
-    while(pont != NULL) { // percorrendo a fila até achar o fim
-        ant = pont;
+Node *insereProcesso(Node *begin) {
+    Node *novo_no = (Node*)malloc(sizeof(Node));
+    novo_no->tempo = rand() % 20 + 1;
+    Node *pont = begin;
+    while(pont->prox != NULL) { // percorrendo a fila até achar o fim
         pont = pont->prox;
     }
-    ant->prox = no;
-    free(pont);
+    pont->prox = novo_no;
     return begin; // retorna o começo da fila com o novo processo inserido no fim
 }
 
+Node *removeProcesso(Node *begin) {
+    if(begin != NULL) {
+        begin = begin->prox;
+    }
+    return begin;
+}
+
 // retorna a chance de um novo processo ser criado
-int criaProcesso() {
+int chanceProcesso() {
     int chance = rand() % 100 + 1;
     if(chance < 30) { // 30% de chance de ser menor que 30
         return 1;
@@ -32,10 +37,14 @@ int criaProcesso() {
 }
 
 // pelo o que eu entendi isso seria o processamento
-void processa(int tempo, int processo) {
+Node *processa(Node *begin, int tempo) {
     for(int i = 0; i < tempo; i++) {
+        int chance = chanceProcesso();
+        if(chance == 1) {
+            begin = insereProcesso(begin);
+        }
     }
-    printf("Processo %d foi executado em %d tempos\n", processo, tempo);
+    return begin;
 }
 
 int main() {
@@ -44,12 +53,13 @@ int main() {
     int processo = 0;
 
     while(true) { // loop infinito
-        int chance = criaProcesso();
+        int chance = chanceProcesso();
         if(chance == 1) { // verifica se deve ser criado um novo processo na iteração
             processo++;
-            Node *novo_no = (Node*)malloc(sizeof(Node));
-            begin = insereProcesso(begin, novo_no);
-            processa(novo_no->tempo, processo);
+            begin = insereProcesso(begin);
+            begin = processa(begin, begin->tempo);
+            printf("Processo %d foi executado em %d tempos\n", processo, begin->tempo);
+            begin = removeProcesso(begin);
         }
     }
 
